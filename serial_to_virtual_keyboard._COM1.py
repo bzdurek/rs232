a1fb483542460d22ab449ae472ad4782e7b2b8a7
bzdurek <bzdurek@users.noreply.github.com>
2023-04-28 14:47:05 +0200

    Update serial_to_virtual_keyboard._COM1.py

--- serial_to_virtual_keyboard._COM1.py
+++ serial_to_virtual_keyboard._COM1.py
@@ -1,17 +1,23 @@
+#pip3 install pynput pyserial
+
 import serial
 from pynput.keyboard import Key, Controller
+import sys
+import glob
 
 
 ser = serial.Serial(
-    port='COM1',    # Number of COM port, change for your system
-    baudrate=9600,  # Port speed
-    timeout=1       # Wait for read
+    port='COM3',    # Numer portu szeregowego (zmień na odpowiedni dla Twojego systemu)
+    baudrate=9600,  # Prędkość transmisji danych
+    timeout=1       # Czas oczekiwania na odczyt danych
 )
 
 
 
 
-print("OK, It work's waiting for data from COM1")
+print("OK, działa - czekam na dane z COM1 - 9600")
+print("Program pobiera wagę ignorując [] i jednostkę, kropkę zamienia na przecinek")
+print("Naciśnij ctrl + c aby wyjść z programu")
 
 keyboard = Controller()
 
@@ -21,9 +27,11 @@ try:
 		if(ser.in_waiting>0):
             
 			rxLine=ser.readline().decode("ascii").strip()
+			# w celu usunięcia z wyników nawiasów [] i jednostki miary trzeba użyć poniższych funkcji 
+			rxLine = rxLine.split(" ")[0].replace("[", "").replace("]", "").replace(".", ",")
 			keyboard.type(rxLine)
 			keyboard.press(Key.enter)
 			keyboard.release(Key.enter)
             
 except:
-	print("Program closed")	
+	print("Koniec pracy programu")
